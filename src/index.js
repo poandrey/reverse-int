2df21eac05e115fa22a00bba892b0e69f955fe27
poandrey <poandrey1985@gmail.com>
2022-06-28 00:47:56 +0300

    fixed:index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 module.exports = function reverse (n) {
-  
+     n = Math.abs(n);
+    // n = n.toString();
+    // if(n.charAt(0) == '-'){n.substring(1)}
+  return n.toString().split('').reverse().join('')
 }
